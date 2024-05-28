02227a969305604ffdee98cc9b16441b051dc96a
TedEngineer <tedvu184@gmail.com>
2024-05-28 21:13:10 +1000

    experiment with writable computed

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+
+var onClick;
 function AppViewModel() {
   this.firstName = ko.observable('Bert');
   this.lastName = ko.observable('Bertington');
 
-  this.fullName = ko.computed(() => {
-    return this.firstName() + ' ' + this.lastName();
+  this.fullName = ko.computed({
+    read: () => {
+      return this.firstName() + ' ' + this.lastName();
+    },
+    write: (value) => {
+      alert('go here ' + value);
+      this.fullName = value;
+    },
+    owner: this,
   });
+
+  onClick = () => {
+    var currentVal = this.fullName(); // Read the current value
+    const uppercaseFullName = currentVal.toUpperCase();
+    alert('debug');
+    this.fullName(uppercaseFullName);
+  };
 }
 
 // Activates knockout.js
